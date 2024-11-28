5d717bc56957b860b6b9df5c2f5f78738135f5eb
RobertCoffey01 <robert.coffey.2022@mumail.ie>
2024-11-28 16:58:54 +0000

    Added CA3_Exercise3.R

---
+++ CA3_Exercise3.R
@@ -0,0 +1,29 @@
+df <- with(mtcars, data.frame(y=mpg, x1=disp, x2=hp, x3=wt))
+df
+
+nll_lm <- function(data, par) {
+  y <- data$y
+  X <- as.matrix(cbind(1, data[, -1]))
+  beta <- par[1:(ncol(X))] 
+  sigma <- par[length(par)] 
+  residuals <- y - X %*% beta
+  llik_first <- dnorm(residuals[1], mean = 0, sd = sigma, log = TRUE)  
+  llik_rest <- sum(dnorm(residuals[-1], mean = 0, sd = sigma, log = TRUE)) 
+  full_llik <- llik_first + llik_rest
+  return(-full_llik)
+}
+inits <- c(mean(df$y), rep(0, ncol(df) - 1), 1) 
+
+fit <- optim(inits, nll_lm, data = df, method = "L-BFGS-B",             
+             lower = c(rep(-Inf, ncol(df)), 1e-3), 
+             upper = c(rep(Inf, ncol(df)), Inf)   
+)
+
+beta_estimates <- fit$par[1:(ncol(df))] 
+sigma_estimate <- fit$par[length(fit$par)] 
+results <- data.frame(
+  Parameter = c("Intercept", "Beta_x1", "Beta_x2", "Beta_x3", "Sigma"),
+  Estimate = round(c(beta_estimates, sigma_estimate), 4)
+)
+
+print(results) 
